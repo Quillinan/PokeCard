8d1cbf0d3c70cb53a7cc41d507591e55e3a0c88e
Quillinan <milton.quillinan@gmail.com>
2023-07-16 15:11:40 -0300

    addToCart

--- src/pages/MenuPage.jsx
+++ src/pages/MenuPage.jsx
@@ -8,6 +8,50 @@ export default function MenuPage() {
   const [cards, setCards] = useState([]);
   const token = localStorage.getItem("token");
 
+  const handleAddToCart = async (card) => {
+    const confirmAddToCart = window.confirm(
+      `Deseja adicionar a carta ${card.name} no seu carrinho?`
+    );
+    if (confirmAddToCart) {
+      console.log(card);
+      try {
+        const response = await axios.post(
+          `${import.meta.env.VITE_API_URL}/cart/add-to-cart`,
+          {
+            cardId: card._id,
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        if (response.status === 200) {
+          alert("Carta adicionada no seu carrinho");
+        }
+      } catch (error) {
+        if (error.response.status === 404) {
+          alert("Carta não encontrada");
+        } else {
+          console.log(error.response.data);
+          // Tratar outros erros aqui
+        }
+      }
+    }
+  };
+
+  const handleRemoveFromCart = async (card) => {
+    const confirmRemoveFromCart = window.confirm(
+      `Deseja remover a carta ${card.name} do seu carrinho?`
+    );
+    if (confirmRemoveFromCart) {
+    }
+    // Remover do carrinho
+  };
+
+  const isInCart = (card) => {};
+  console.log(token);
+
   useEffect(() => {
     const fetchCards = async () => {
       try {
@@ -38,10 +82,15 @@ export default function MenuPage() {
       <SearchBar />
       <CardContainer>
         {cards.map((card) => (
-          <Card key={card._id}>
+          <Card key={card._id} onClick={() => handleAddToCart(card)}>
             <h2 className="name">{card.name}</h2>
-            <img src={"PikachuImage.svg"} alt="" />
+            <img className="cardImg" src={"PikachuImage.svg"} alt="" />
             <h2>R$ {card.value}</h2>
+            {isInCart(card) && (
+              <div className="overlay" onClick={handleRemoveFromCart}>
+                <img src="Multiply.svg" alt="" />
+              </div>
+            )}
           </Card>
         ))}
       </CardContainer>
@@ -100,6 +149,7 @@ const Card = styled.div`
   background: #fff;
   border-radius: 15px;
   border: 1px solid #000;
+  position: relative;
 
   width: 150px;
   height: 200px;
@@ -107,9 +157,24 @@ const Card = styled.div`
     align-self: flex-start;
     margin-left: 10px;
   }
-  img {
+  .cardImg {
     border: #000 1px solid;
     width: 100%;
     height: 70%;
+  }
+  .overlay {
+    position: absolute;
+    top: -3%;
+    right: -8%;
+    width: 30px;
+    height: 30px;
+    background-color: #ffc7c7;
+    border-radius: 50%;
+    cursor: pointer;
+    img {
+      width: 100%;
+      height: 100%;
+      rotate: 45deg;
+    }
   }
 `;
